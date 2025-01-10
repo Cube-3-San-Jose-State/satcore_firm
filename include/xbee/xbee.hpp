015490732ab95664c96191925de965ca45803b64
Andy Lazcano <andylazcanop@gmail.com>
2025-01-10 04:42:11 -0800

    small changes to register file and started class.

---
+++ include/xbee/xbee.hpp
@@ -0,0 +1,7 @@
+#include <libhal/i2c.hpp>
+#include <libhal-util/i2c.hpp>
+#include "xbee/xbee_reg.hpp"
+
+class xbee{
+    
+}
--- include/xbee/xbee_reg.hpp
+++ include/xbee/xbee_reg.hpp
@@ -10,14 +10,17 @@ part number.
 */ 
 namespace xbee
 {
-    static constexpr hal::byte STATUS         = 0x00;  
-    static constexpr hal::byte TX_REQUEST     = 0x01;  
-    static constexpr hal::byte RX_INDICATOR   = 0x02;  
-    static constexpr hal::byte AT_COMMAND     = 0x03;  
+    static constexpr hal::byte STATUS          = 0x00;  
+    static constexpr hal::byte TX_REQUEST      = 0x01;  
+    static constexpr hal::byte RX_INDICATOR    = 0x02;  
+    static constexpr hal::byte AT_COMMAND      = 0x03;  
     //static constexpr hal::byte MODEM_STATUS   = 0x04;  
-    static constexpr hal::byte TX_STATUS      = 0x05;  
-    static constexpr hal::byte ROUTE_RECORD   = 0x06;  
-    static constexpr hal::byte TX_PASS        = 0x07;  
-    static constexpr hal::byte DEFAULT_ADDRESS= 0x0C;
-   
+    static constexpr hal::byte TX_STATUS       = 0x05;  
+    static constexpr hal::byte ROUTE_RECORD    = 0x06;  
+    static constexpr hal::byte TX_PASS         = 0x07;  
+    static constexpr hal::byte DEFAULT_ADDRESS = 0x0C;
+    static constexpr hal::byte START_DELIMITER = 0x7E;
+    static constexpr size_t MAX_FRAME_SIZE     = 256;
+    static constexpr size_t CHECKSUM_SIZE      = 1;
+    static constexpr size_t LENGTH_SIZE        = 2;
 };
